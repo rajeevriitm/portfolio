07776490b5576a0917044cf9c2e678346ae73eae
rajeev <rajeevriitm@gmail.com>
2016-08-06 00:20:25 +0530

    modal, http auth,resume

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :initial, only: [:edit,:update,:destroy,:show]
-  before_action :admin, only: [:edit,:new]
+  http_basic_authenticate_with name:"raj", password: ENV['PASSWORD'], only: [:edit,:new, :create,:update]
   before_action :set_meta,only: :show
   caches_page :show,:index
   def new
@@ -47,9 +47,9 @@ class BlogsController < ApplicationController
   def initial
     @blog=Blog.find_by(id: params[:id])
   end
-  def admin
-    redirect_to home_path unless params[:admin] == "raj"
-  end
+  # def admin
+  #   redirect_to home_path unless params[:admin] == ENV["PASSWORD"]
+  # end
   def set_meta
     set_meta_tags description: @blog.title,
     keywords: 'Freelance, web-developer, Ruby on Rails',
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-  has_many :comments
+  has_many :comments,dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :content,presence: true,uniqueness: {case_sensitive: false}
   validates :title,presence: true
