c72a5acd1f67110c8ec818b884d1b8b88eb38f4a
rajeev <rajeevriitm@gmail.com>
2016-08-06 17:42:34 +0530

    authentication token disabled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :null_session
+  helper_method :time_of_creation
+  include ActionView::Helpers::DateHelper
   def time_of_creation(date)
     date < 24.hours.ago ? date.strftime("%m/%d/%Y") : time_ago_in_words(date) +" ago"
   end
